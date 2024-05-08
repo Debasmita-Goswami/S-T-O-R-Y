d1a9de102472ceb0c7387716cca9c278d0d2ac14
Ayushi Srivastava <ayushirensa@gmail.com>
2024-05-08 21:30:57 +0530

    Google login done

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,6 +2,8 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 
 export const authOptions = {
